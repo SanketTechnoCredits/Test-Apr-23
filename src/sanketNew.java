90b12032a505a66b169ac929a7be3439787012db
SanketTechnoCredits <sanketthakur1995@gmail.com>
2023-05-23 08:38:52 +0530

    update in Stringprac file

--- src/sanketNew.java
+++ src/sanketNew.java
@@ -3,6 +3,6 @@ public class sanketNew {
 	
 	public static void main(String[] args) {
 		System.out.println("Hello");
-		System.out.println("creating conflict scenario from A");
+		System.out.println("conflict resolved");
 	}
 }
